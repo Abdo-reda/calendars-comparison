3712c060ba0e371381cd5121b4a05d51090f9f83
Abdo-reda <abdo.reda@aucegypt.edu>
2025-03-07 00:16:54 +0200

    fixed styling for years, and added hijri support

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,74 @@
 <script setup lang="ts">
-import { computed, nextTick, onMounted, reactive, ref, useTemplateRef, watch } from 'vue';
+import { computed, onMounted, ref, useTemplateRef } from 'vue';
 import { gsap } from "gsap";
+import { toHijri } from 'luxon-hijri';
+import { formatHijriDate } from 'luxon-hijri';
 
-const tl = gsap.timeline({ paused: true }); // Pause the timeline initially
+//---- TODO
+//- start of week color? start of week and month different.
+//- tooltips when you hover over the day.
+
+interface IDay {
+  locale: string;
+  short: string;
+  month: string;
+  isStartOfMonth: boolean;
+  isStartOfWeek: boolean;
+}
+
+interface ICalendarDays {
+  days: IDay[],
+  hijriDays: IDay[]
+}
+
+// const dateFormatter = new Intl.DateTimeFormat('default', { month: 'long' });
+const daySize = 24;
+const dayGap = 12;
+const currentYear = new Date().getUTCFullYear()
+const yearsSequence = gsap.timeline({ paused: true });
+const years = ref([currentYear - 1, currentYear, currentYear + 1]);
+const activeMonth = ref<string | null>(null);
+const highlightWeek = ref<boolean>(false);
+const mainContainer = useTemplateRef('main-container');
+
+const hijriYears = computed(() => {
+  return years.value.map(y => {
+    const currentYearDate = new Date(y, 0, 1);
+    return toHijri(currentYearDate)?.hy;
+  })
+})
+
+const days = computed<ICalendarDays>(() => {
+  const yearDays: IDay[] = [];
+  const hijriYearDays: IDay[] = [];
+  for (const y of years.value) {
+    const currentYearDate = new Date(y, 0, 1);
+    while (currentYearDate.getFullYear() === y) {
+      const hijriDate = toHijri(currentYearDate);
+      yearDays.push({
+        locale: currentYearDate.toLocaleDateString(),
+        short: currentYearDate.toISOString().split('T')[0],
+        month: currentYearDate.toLocaleString('default', { month: 'long' }),
+        isStartOfMonth: currentYearDate.getDate() === 1,
+        isStartOfWeek: currentYearDate.getDay() === 0,
+      });
+      if (hijriDate) {
+        hijriYearDays.push({
+          locale: formatHijriDate(hijriDate, 'iYYYY-iMM-iDD'),
+          short: formatHijriDate(hijriDate, 'iYYYY-iMM-iDD'),
+          month: formatHijriDate(hijriDate, 'iMMMM'),
+          isStartOfMonth: hijriDate.hd === 1,
+          isStartOfWeek: currentYearDate.getDay() === 0,
+        })
+      }
+      currentYearDate.setDate(currentYearDate.getDate() + 1);
+    }
+  }
+  return {
+    days: yearDays,
+    hijriDays: hijriYearDays,
+  }
+})
 
 //-------> each one will have a set animation, a sequence of tweens, and once a sequence completes, we should only need to keep track of two sequences (prev year, next year), only one year can be visible at a time.
 //--> building that sequence might be a pain in the ass ... hmm .... is there a better way to deal with it?
@@ -10,17 +76,13 @@ const tl = gsap.timeline({ paused: true }); // Pause the timeline initially
 // Variable to control the animation progress
 
 // Function to update the animation progress
-function updateAnimation(t: number) {
-  console.log('------ update animation', t)
-  tl.totalProgress(t); // Set the timeline's progress based on `t`
-}
+
 
 const VIEWPORT_CENTER = window.innerWidth / 2;
 const YEAR_RANGE = 1;
 const CENTER_RANGE = 300;
-const currentYear = ref<number>(new Date().getUTCFullYear())
 // const activeYear = ref(currentYear.value);
-const years = computed(() => Array.from({ length: YEAR_RANGE * 2 + 1 }, (_, i) => currentYear.value + i - YEAR_RANGE))
+// const years = computed(() => Array.from({ length: YEAR_RANGE * 2 + 1 }, (_, i) => currentYear.value + i - YEAR_RANGE))
 // const list = reactive(Array.from({ length: 5000 }, (_, index) => {
 //   return {
 //     id: index,
@@ -66,70 +128,84 @@ const years = computed(() => Array.from({ length: YEAR_RANGE * 2 + 1 }, (_, i) =
 //   // await nextTick();
 // }
 
-//TODO:
-//- fix styling
-//- make the list computed (memory is user memory)
-//- make it horizontal
-//- see into vue transitions.
-
-const listLeft = ref()
-
 onMounted(() => {
-  // if (yearContainer.value) {
-  //   // yearContainer.value.scrollLeft = yearContainer.value.scrollWidth/2;
-  //   yearContainer.value.scrollLeft = (yearContainer.value.scrollWidth - yearContainer.value.clientWidth) / 2;
-  // }
-  // handleScroll();
+  scrollToMiddleYear();
   defineYearAnimation();
 })
 
-function defineYearAnimation() {
-  tl.fromTo(".box",
-    { x: "100vw" },
-    {
-      x: "64px",
-      ease: "power1.inOut"
-    }
-  ).to(".box",
-    {
-      scale: 0,
-      opacity: 0,
-      ease: "power1.inOut"
-    },
-    "+=1"
-  );
+function onDayClick(d: IDay) {
+  navigator.clipboard.writeText(d.locale);
 }
 
-function onDayClick() {
-  console.log('---- clickedo n day')
+function scrollToMiddleYear() {
+  console.log("--- hello?")
+  if (!mainContainer.value) return;
+  mainContainer.value.scrollLeft = daysInYear(years.value[0]) * (daySize + dayGap) - dayGap;
+}
+
+function daysInYear(year: number) {
+  return ((year % 4 === 0 && year % 100 > 0) || year % 400 == 0) ? 366 : 365;
+}
+
+function defineYearAnimation() {
+  yearsSequence
+    .fromTo(".box",
+      { x: "100vw" },
+      {
+        x: "64px",
+        ease: "power1.inOut"
+      }
+    ).to(".box",
+      {
+        scale: 0,
+        opacity: 0,
+        ease: "power1.inOut"
+      },
+      "+=1"
+    );
+}
+
+function updateAnimation(t: number) {
+  yearsSequence.totalProgress(t);
 }
 
 </script>
 
 <template>
-  <header class="year-container">
-    <!-- <p style="color: white"> {{ currentYear }} {{ yearContainer?.scrollLeft }} </p>
-    <p v-for="temp in listLeft" :key="temp" style="color: white"> {{ temp }} </p>
-    <div class="year-container" ref="yearContainer" @scroll="handleScroll">
-      <h1 class="year" v-for="y in years" :key="y" :class="{ 'sticky-year': y === currentYear }" ref="yearHeading"> {{ y
-        }}</h1>
-    </div> -->
-    <div class="box"> </div>
-    <!-- <RecycleScroller class="years-container" :pageMode="true" :items="years" :item-size="32" direction='horizontal'  v-slot="{ item }">
-      <h1> {{ item.value }}</h1>
-    </RecycleScroller> -->
-  </header>
+  <div class="body-wrapper">
+    <header>
+      <h1> {{ years }} </h1>
+    </header>
+    <main ref="main-container" class="main-container">
+      <div class="days-container" ref="days-container">
+        <template v-for="d in days.days" :key="d.short">
+          <div class="day-container">
+            <p class="month-title month-title-milady" :class="{ 'month-title-hover': activeMonth === d.month }"
+              v-if="d.isStartOfMonth"> {{
+                d.month }} </p>
+            <div @click="onDayClick(d)" @mouseenter="activeMonth = d.month" @mouseleave="activeMonth = null" class="day"
+              :class="{ 'day-first-month': d.isStartOfMonth, 'day-normal': !d.isStartOfMonth }">
+            </div>
+          </div>
+        </template>
+      </div>
+      <div class="days-container" ref="days-container">
+        <template v-for="d in days.hijriDays" :key="d.short">
+          <div class="day-container">
+            <p class="month-title month-title-hijri" :class="{ 'month-title-hover': activeMonth === d.month }"
+              v-if="d.isStartOfMonth"> {{
+                d.month }} </p>
+            <div @click="onDayClick(d)" @mouseenter="activeMonth = d.month" @mouseleave="activeMonth = null" class="day"
+              :class="{ 'day-first-month': d.isStartOfMonth, 'day-normal': !d.isStartOfMonth }">
+            </div>
+          </div>
+        </template>
+      </div>
+    </main>
 
-  <main>
-    <div class="day-container">
-      <div @click="onDayClick" class="day" :class="{ 'day-first-month': i % 30 === 0, 'day-normal': i % 30 !== 0 }"
-        v-for="i in 365" :key="i">
-      </div>
-    </div>
-    <!-- <RecycleScroller class="scroller" :pageMode="true" :items="list" :item-size="32" key-field="id" v-slot="{ item }">
-      <div class="user">
-        {{ item.name }}
-      </div>
-    </RecycleScroller> -->
-  </main>
+    <footer>
+      <h1> {{ hijriYears }} </h1>
+    </footer>
+  </div>
+
 </template>
